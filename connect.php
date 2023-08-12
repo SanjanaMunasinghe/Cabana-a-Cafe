f3fb5d9051340bd1cd9e34abb8785270a95a54e5
SanjanaMunasinghe <136359091+SanjanaMunasinghe@users.noreply.github.com>
2023-08-12 21:36:10 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+ $first name = $_post['first name'];
+ $last name = $_post['last name'];
+ $gender = $_post['gender'];
+ $email = $_post['email'];
+ $password = $_post['password'];
+ $number = $_post['number'];
+
+ //data base connection
+
+ 
